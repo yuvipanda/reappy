aa56603d1e4a3abe4ae4e378eba3524228b6f305
yuvi <yuvipanda@trillbone.(none)>
2009-12-20 15:06:21 +0530

    Seperated Application into Search Application

--- reappy.py
+++ reappy.py
@@ -12,15 +12,7 @@ except ImportError:
 
 USER_AGENT = "reappy application/0.1"
 
-def _grab_results(search_url):
-    req = Request(search_url, headers={'User-Agent': USER_AGENT})
-    results = json.loads(urlopen(req).read())
-    return results
 
-def _build_url(search_query, since_id=None):
-    params = {'q':search_query, 'rpp':100}
-    if since_id: params['since_id'] = since_id
-    return "http://search.twitter.com/search.json?" + urlencode(params)
 
 class User:
     def __init__(self, data):
@@ -36,9 +28,9 @@ class Tweet:
         self.language = data['iso_language_code']
         self.created = parser.parse(data['created_at'])
 
+    
 class Application():
-    def __init__(self, search_term, handlers):
-        self.search_term = search_term
+    def __init__(self, handlers):
         self.handlers = [(re.compile(r[0], re.IGNORECASE), r[1]) for r in handlers]
         if os.path.exists('since_id.data'):
             since_id_file = file('since_id.data', 'r')
@@ -51,9 +43,11 @@ class Application():
         cPickle.dump(self.since_id, save_file)
         save_file.close()
 
+    def get_tweets_since(self, since_id):
+        pass
+    
     def run(self):
-        url = _build_url(self.search_term, since_id=self.since_id)
-        tweets = [Tweet(rd) for rd in _grab_results(url)['results']]
+        tweets = self.get_tweets_since()
         if tweets:
             self.since_id = tweets[0].id
             self._persist_since_id()
@@ -68,3 +62,27 @@ class Application():
         while True:
             self.run()
             time.sleep(poll_frequency)
+
+class SearchApplication(Application):
+    def __init__(self, search_term, handlers):        
+        Application.__init__(self, handlers)
+        self.search_term = search_term
+        
+    def _persist_since_id(self):
+        save_file = file('since_id.data', 'w')
+        cPickle.dump(self.since_id, save_file)
+        save_file.close()
+        
+    def _grab_results(self, search_url):
+        req = Request(search_url, headers={'User-Agent': USER_AGENT})
+        results = json.loads(urlopen(req).read())
+        return results
+
+    def _build_url(self, search_query, since_id=None):
+        params = {'q':search_query, 'rpp':100}
+        if since_id: params['since_id'] = since_id
+        return "http://search.twitter.com/search.json?" + urlencode(params)
+
+    def get_tweets_since(self):        
+        url = self._build_url(self.search_term, since_id=self.since_id)
+        return [Tweet(rd) for rd in self._grab_results(url)['results']]
